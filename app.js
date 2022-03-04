ee8acae853df7a8fca36c7c4a06ae26dd675cd70
Saint Gabriel <53136855+chineduG@users.noreply.github.com>
2022-03-04 04:21:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 // ! Modules
 
+const isAge = 59;
+cosnsole.log('Code being here');
+if(isAge < 50){
+  console.log('Small Age');
+}else{
+    console.log('Higher Age')
+ }
+console.log('End of the Code');
+         
+
